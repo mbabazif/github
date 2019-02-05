455afa3359368d79fb54f97ddebbfbbe312cc424
Florence <mbababziflorence81@gmail.com>
2019-02-05 17:28:39 +0200

    adding githubsearch

--- src/app/usres/user.service.ts
+++ src/app/usres/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-// import 'rxjs/add/operator/map';
 
 import { Users } from '../users';
 
@@ -19,13 +18,7 @@ export class UserService {
     console.log('Userservice')
     this.username =""
   }
-//   getUser(){
-//     return this._http.get('https://api.github.com/users/' + this.username)
-//     .data(result => result);
-//   }
-//   updateUser(username:string){
-// this.username = username;
-//   }
+
 
 }
 
